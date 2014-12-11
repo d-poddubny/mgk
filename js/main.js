2b246def064c5da453374a7961d9290eb9d669b8
d-poddubny <css.experts@gmail.com>
2014-12-12 01:45:33 +0300

    index & inner page

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+
+    $('#news-carousel1').carousel()
+    $('#news-carousel2').carousel({ itemsToScroll: 8 })
+    $('#peoples').clicker({
+        item: '.peoples_item'
+    })
+
+})
